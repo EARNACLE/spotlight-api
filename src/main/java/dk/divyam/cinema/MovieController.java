150bec1f7f37c6ebcbbc4ab68ce2dba093549653
DivyamKakkar24 <kakkardivyam24@gmail.com>
2023-05-24 09:42:59 +0530

    crossorigin fixed

--- src/main/java/dk/divyam/cinema/MovieController.java
+++ src/main/java/dk/divyam/cinema/MovieController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = {"http://localhost:3000"})
+@CrossOrigin(origins = {"https://spotlight-movie.netlify.app/"})
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
--- src/main/java/dk/divyam/cinema/ReviewController.java
+++ src/main/java/dk/divyam/cinema/ReviewController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-@CrossOrigin(origins = {"http://localhost:3000"})
+@CrossOrigin(origins = {"https://spotlight-movie.netlify.app/"})
 @RestController
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
