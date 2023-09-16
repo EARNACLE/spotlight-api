d7818ac2dc8731cc64c9f784ae1f30e3fd292af1
DivyamKakkar24 <kakkardivyam24@gmail.com>
2023-09-16 16:21:52 +0530

    testing db

--- src/main/java/dk/divyam/cinema/CinemaApplication.java
+++ src/main/java/dk/divyam/cinema/CinemaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class CinemaApplication {
-
+//	don't push code
 
 	public static void main(String[] args) {
 		SpringApplication.run(CinemaApplication.class, args);
